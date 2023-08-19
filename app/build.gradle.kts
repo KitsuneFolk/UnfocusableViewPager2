61578fcfd6f8077d6bb5899bf282dd35fb7c518e
MrRuslanYT <pandacorpglobal@gmail.com>
2023-08-19 15:02:33 +0300

    Removed tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,6 @@ android {
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -44,11 +42,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.9.0")
-
-    // Tests
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     // Modules
     implementation(project(":viewpager2"))
