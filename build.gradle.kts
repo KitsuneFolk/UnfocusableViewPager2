5d9950d2baf953180e651a2204f470a6d85d84e2
MrRuslanYT <pandacorpglobal@gmail.com>
2023-08-20 14:42:22 +0300

    Added ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.android.library") version "8.1.0" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.5.1" apply false
 }
+apply(from = ("gradle/ktlint.gradle.kts"))
